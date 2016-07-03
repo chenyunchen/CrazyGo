d2675a6c37e10e2115c2f8f3aa95f1e97f3c74a8
chenyunchen <yunchen@nisra.net>
2016-07-04 04:49:38 +0800

    Make context for middleware

--- main.go
+++ main.go
@@ -9,16 +9,39 @@ import (
 
 type Handler func(next http.Handler) http.Handler
 
+// context
+// ----------------------------------------------------------
+type context struct {
+	data map[*http.Request]map[interface{}]interface{}
+}
+
+func (con *context) Set(r *http.Request, key, value interface{}) {
+	_, ok := con.data[r]
+	if !ok {
+		con.data[r] = make(map[interface{}]interface{})
+	}
+	con.data[r][key] = value
+
+}
+func (con context) Get(r *http.Request, key interface{}) interface{} {
+	return con.data[r][key]
+}
+
+// Make just init outer map!
+var c = context{make(map[*http.Request]map[interface{}]interface{})}
+
+// ----------------------------------------------------------
+
 // Merge Multiple Handler
 // ==========================================================
 type Execute struct {
 	fn func(http.ResponseWriter, *http.Request)
 }
 
-func (h Execute) Check(x []Handler) http.Handler {
+func (ex Execute) Check(hs ...Handler) http.Handler {
 	var chain http.Handler
-	chain = http.HandlerFunc(h.fn)
-	for _, fn := range x {
+	chain = http.HandlerFunc(ex.fn)
+	for _, fn := range hs {
 		chain = fn(chain)
 	}
 	return chain
@@ -52,14 +75,28 @@ func errorH(next http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
+//Auth Example
+func authH(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// Do something check user identity
+		user := make(map[string]string)
+		user["name"] = "test"
+		c.Set(r, "user", user)
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
 // ----------------------------------------------------------
 
 // Test Example for Handlers
 func test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test")
+	user := c.Get(r, "user").(map[string]string)
+	fmt.Println("Name:", user["name"])
 }
 
 func main() {
-	http.Handle("/", Execute{test}.Check([]Handler{logH, errorH}))
+	http.Handle("/test", Execute{test}.Check(logH, errorH, authH))
 	http.ListenAndServe(":8080", nil)
 }
